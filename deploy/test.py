053ff408e2da1e80c02240aa13f539f6bc736007
tienthienhd <tienthienhd@gmail.com>
2019-03-21 00:14:21 +0700

    Update test.py

--- deploy/test.py
+++ deploy/test.py
@@ -7,7 +7,7 @@ import numpy as np
 parser = argparse.ArgumentParser(description='face model test')
 # general
 parser.add_argument('--image-size', default='112,112', help='')
-parser.add_argument('--model', default='pretrain_model/model,0', help='path to load model.')
+parser.add_argument('--model', default='/content/pretrain_model/model,0', help='path to load model.')
 parser.add_argument('--ga-model', default='', help='path to load model.')
 parser.add_argument('--gpu', default=0, type=int, help='gpu id')
 parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
