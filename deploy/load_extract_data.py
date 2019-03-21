f57367517e1e63b2f243ba0c8629863c0b77d145
tienthien <tienthienhd@gmail.com>
2019-03-21 08:48:00 +0700

    fix path data

--- deploy/load_extract_data.py
+++ deploy/load_extract_data.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 
-train = pd.read_csv('../../vn_celeb_face_recognition/train.csv')
+train = pd.read_csv('/content/data/train.csv')
 # print(train.head())
 
 
@@ -30,7 +30,7 @@ images = []
 labels = []
 
 for i,row in train.iterrows():
-    image_path = '../../vn_celeb_face_recognition/train/' + row['image']
+    image_path = '/content/data/train/' + row['image']
     label = row['label']
 
     try:
